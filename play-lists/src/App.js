a3e3e2ccfd06c810f7956f615a2e1a468f39d846
tony2tones <anthony.marque@gmail.com>
2018-05-08 09:56:24 +0200

    updating app styling

--- play-lists/src/App.js
+++ play-lists/src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let name = 'Tony';
+    let green = '#ffff';
+    let headerStyle = {color:green, 'font-size': '50px'}
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome, Tony</h1>
+          <h1 style = {{headerStyle}}>Welcome, {name}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
