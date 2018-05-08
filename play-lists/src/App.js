4ad215011a2e95aad36a8166763184e77284da3a
tony2tones <anthony.marque@gmail.com>
2018-05-08 10:28:31 +0200

    added playlist

--- play-lists/src/App.js
+++ play-lists/src/App.js
@@ -2,20 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{ width: '40%', display: 'inline-block' }}>
+        <h2>Number Text </h2>
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <img />
+        <input type='text' />
+      </div>
+    )
+  }
+}
+
+class PlayList extends Component {
+  render() {
+    return (
+      <div style={{width: '25%'}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     let name = 'Tony';
     let green = '#ffff';
-    let headerStyle = {color:green, 'font-size': '50px'}
+    let headerStyle = { color: green, 'font-size': '50px' }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 style = {{headerStyle}}>Welcome, {name}</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Title</h1>
+        <Aggregate />
+        <Aggregate />
+        <Search />
+        <PlayList />
+        <PlayList />
+        <PlayList />
+        
       </div>
     );
   }
